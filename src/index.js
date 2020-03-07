f54976dece14b9afef3732927e4d42d52bfd46dd
Andy Wong <andywong86@gmail.com>
2020-03-07 14:52:57 -0500

    Add some npm scripts

--- src/index.js
+++ src/index.js
@@ -4,8 +4,43 @@ type Point2d = {|
   y: number
 |};
 const myPoint: Point2d = {
-  X: 1,
+  x: 1,
   y: 2
 };
 console.log(myPoint.x, myPoint.y);
 
+class shape {
+  color: string
+  constructor(color) {
+    this.color = color;
+  }
+
+  getColor() {
+    return this.color;
+  }
+
+}
+
+class square extends shape{
+  length: number
+  constructor(color, length) {
+    super(color);
+    this.length = length;
+  }
+
+  area() {
+    let a = this.length * this.length; 
+    console.log('Area of this ' + this.getColor() + ' square is ' + a);
+    return a;
+  }
+
+  perimeter() {
+    return this.length * 4;
+  }
+
+}
+
+let sq = new square("red",10);
+
+console.log(sq.area());
+console.log(sq.perimeter());
